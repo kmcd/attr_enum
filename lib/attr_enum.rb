195d53235516fcce5c85fc321c4e2453afb0f37f
Keith McDonnell <keith@dancingtext.com>
2009-04-23 17:24:51 +0200

    Added rdoc documentation

--- lib/attr_enum.rb
+++ lib/attr_enum.rb
@@ -1,26 +1,54 @@
-class Module
-  # A macro to define an enumeration variable
-  # See http://en.wikipedia.org/wiki/Enumerated_type for more info
-  # 
-  # class Card
-  #   attr_enum :suit, %w( clubs hearts diamonds spades )
-  # end
-  # 
-  # c = Card.new
-  # c.suit = 'clubs'
-  # c.suit
-  # => 'clubs'
-  # m.suit = 'jack' 
-  # => EnumeratedTypeError, Card.suit must be one of clubs, hearts, diamonds, spades.
+# = attr_enum
+# A macro to define an enumeration variable
+# See http://en.wikipedia.org/wiki/Enumerated_type for more info
+# = Usage
+#   require 'rubygems'
+#   require 'attr_enum'
+# 
+#   class Card
+#     attr_enum :suit, %w( clubs hearts diamonds spades )
+#   end
+# 
+#   c = Card.new
+#   c.suit = 'clubs'                                                                    
+#   c.suit
+#   => 'clubs'
+#   m.suit = 'jack' 
+#   => EnumeratedTypeError, Card.suit must be one of clubs, hearts, diamonds, spades.
+
+class Module                                  
+  
+  # Declare an enumerated type which creates:
+  # * a class constant
+  # * a reader attribute
+  # * a writer with type constraint
+  # === Arguments
+  # +name+:: the name of the enumerated type, e.g :suit                                      
+  # +types+:: an array of valid types, e.g. %w( clubs hearts diamonds spades )
+  # === Example
+  #   class Card
+  #     attr_enum :suit, %w( clubs hearts diamonds spades )
+  #   end
+  # ==== Class constant
+  #   Card::SUITS
+  #   => ["clubs", "hearts", "diamonds", "spades"]
+  # ==== reader/writer attributes
+  #   c = Card.new
+  #   c.suit = 'clubs'
+  #   c.suit
+  #   => 'clubs'
+  # ==== Error handling:
+  #   Card.new.suit = 'spades' 
+  #   => EnumeratedTypeError, Card.suit must be one of clubs, hearts, diamonds, spades.                                               
   def attr_enum(name, types)
-    attr_reader name
-    
+    attr_reader name                                                                  
+                                                                                                          
     class_eval do
-      # Create a class constant
+      # Create a class constant                                                          
       const_set :"#{name.to_s.upcase}_TYPES", types
       
       define_method(:"#{name}=") do |type|
-        # Add type constraint to setter method
+        # Add type constraint to setter method                                          
         if self.class.const_get(:"#{name.to_s.upcase}_TYPES").include?(type)
           instance_variable_set :"@#{name}", type
         else
